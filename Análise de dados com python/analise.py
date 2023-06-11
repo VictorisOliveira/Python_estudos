49fc6b11d904244150d09d4c25bef32df285a019
Victor I S Oliveira <70918081+VictorisOliveira@users.noreply.github.com>
2023-06-11 13:49:19 -0300

    Add files via upload

---
+++ Análise de dados com python/analise.py
@@ -0,0 +1,9 @@
+x = 10
+nome = 'aluno'
+nota = 10
+fez_inscricao = True
+
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(fez_inscricao))
---
+++ Análise de dados com python/hello.py
@@ -0,0 +1,11 @@
+a = 2
+b = 0.5
+c = 1
+x = input("Digite o valor de x: ")
+
+x = float(x) #aqui fazemos a conversão da string numérico
+
+y = a * x ** 2 + b * x + c
+
+print(f"O resultado de y para x = {x} é {y}.")
+
